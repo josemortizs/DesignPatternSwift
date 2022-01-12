df3bd48a6a9a384807b8143a9c5665cd20c9a065
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2022-01-12 19:43:15 +0100

    Initial Commit

---
+++ DesignPatternSwiftUI/DesignPatternSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternSwiftUIApp.swift
+//  DesignPatternSwiftUI
+//
+//  Created by Jose Manuel Ortiz Sanchez on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
